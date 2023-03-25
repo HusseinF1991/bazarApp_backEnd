10863a2213202f23e1050b6bfc8cf339a57eac1e
HusseinF1991 <49164310+HusseinF1991@users.noreply.github.com>
2023-03-25 16:01:58 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "u7FjW!xfDG@4qd",
+  password: "",
   database: "bazarapp"
 });
 
